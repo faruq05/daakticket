2662c2d938a8dc365cbeba4a3f2fa03c83b9eb6a
faruq05 <faruq05.fiverr@gmail.com>
2024-11-15 09:52:47 +0600

    sidebar

--- header.php
+++ header.php
@@ -20,9 +20,14 @@ include 'db.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <!-- stylesheet -->
     <link rel="stylesheet" href="assets/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/sidebar-style.css">
     <link rel="stylesheet" href="assets/css/media.css">
     <!-- database connect -->
 </head>
--- user_dashboard.php
+++ user_dashboard.php
@@ -1,26 +1,127 @@
-<?php 
-include 'header.php'; 
+<?php
+include 'header.php';
 ?>
 
-<div class="dashboard">
-    <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-12">
-                <?php if (isset($_SESSION['user_id']) && isset($_SESSION['username'])): ?>
-                    <!-- Display the welcome message if logged in -->
-                    <h2>Welcome to Your Dashboard, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h2>
-                    
-                    <!-- Logout Button -->
-                    <form action="logout.php" method="POST">
-                        <button type="submit" name="logout" class="btn btn-danger mt-2">Logout</button>
-                    </form>
-                <?php else: ?>
-                    <!-- Redirect or display message if not logged in -->
-                    <p>You are not logged in. Please <a href="login.php">login</a> to access the dashboard.</p>
-                <?php endif; ?>
+
+<div class="col-md-12 wrapper">
+    <aside id="sidebar">
+        <div class="d-flex">
+            <button class="toggle-btn" type="button">
+                <i class="lni lni-grid-alt"></i>
+            </button>
+            <div class="sidebar-logo">
+                <a href="#"> <?php if (isset($_SESSION['user_id']) && isset($_SESSION['username'])): ?>
+                        <p><?php echo htmlspecialchars($_SESSION['username']); ?></p>
+                    <?php endif; ?>
+                </a>
+            </div>
+        </div>
+        <ul class="sidebar-nav">
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-user"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-agenda"></i>
+                    <span>Posts</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-agenda"></i>
+                    <span>Media</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-agenda"></i>
+                    <span>Activity Log</span>
+                </a>
+            </li>
+            <!-- <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse" data-bs-target="#auth"
+                    aria-expanded="false" aria-controls="auth">
+                    <i class="lni lni-protection"></i>
+                    <span>Posts</span>
+                </a>
+                <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">Login</a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">Register</a>
+                    </li>
+                </ul>
+            </li> -->
+
+            <!-- <li class="sidebar-item">
+                <a href="#" class="sidebar-link collapsed has-dropdown" data-bs-toggle="collapse"
+                    data-bs-target="#multi" aria-expanded="false" aria-controls="multi">
+                    <i class="lni lni-layout"></i>
+                    <span>Multi Level</span>
+                </a>
+                <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-two"
+                            aria-expanded="false" aria-controls="multi-two">
+                            Two Links
+                        </a>
+                        <ul id="multi-two" class="sidebar-dropdown list-unstyled collapse">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Link 1</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Link 2</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li> -->
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-popup"></i>
+                    <span>Notification</span>
+                </a>
+            </li>
+            <li class="sidebar-item">
+                <a href="#" class="sidebar-link">
+                    <i class="lni lni-cog"></i>
+                    <span>Change Paassword</span>
+                </a>
+            </li>
+        </ul>
+        <div class="sidebar-footer">
+            <a href="#" class="sidebar-link">
+                <i class="lni lni-exit"></i>
+                <span>Logout</span>
+            </a>
+        </div>
+    </aside>
+
+    <div class=" main dashboard">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-12">
+                    <?php if (isset($_SESSION['user_id']) && isset($_SESSION['username'])): ?>
+                        <!-- Display the welcome message if logged in -->
+                        <h2>Welcome to Your Dashboard, <?php echo htmlspecialchars($_SESSION['username']); ?>!</h2>
+
+                        <!-- Logout Button -->
+                        <form action="logout.php" method="POST">
+                            <button type="submit" name="logout" class="btn btn-danger mt-2">Logout</button>
+                        </form>
+                    <?php else: ?>
+                        <!-- Redirect or display message if not logged in -->
+                        <p>You are not logged in. Please <a href="login.php">login</a> to access the dashboard.</p>
+                    <?php endif; ?>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 
+</div>
 <?php include 'footer.php'; ?>
