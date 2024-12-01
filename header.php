5d054b89fc05c8c05f3fb303857c7a505311a6f3
faruq05 <faruq05.fiverr@gmail.com>
2024-12-01 19:42:49 +0600

    favicon

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ include 'db.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>DaakTicket</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <!-- font -->
@@ -27,6 +27,7 @@ include 'db.php';
     <link href="https://cdn.lineicons.com/5.0/lineicons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="icon" href="assets/uploads/favicon.png">
 
     <!-- stylesheet -->
     <link rel="stylesheet" href="assets/css/all.min.css">
